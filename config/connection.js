f6689273e6dc77de01d8e3ee3e56f84cdd5cd2a1
kelz <kelsie.langston@yahoo.com>
2020-02-10 22:11:49 -0500

    add heroku msql environment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 require("dotenv").config("../");
 const mysql = require("mysql");
+let connnection; 
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: process.env.PSWD,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: process.env.PSWD,
+        database: "burgers_db"
+    });
+}
 
+connection.connect();
 module.exports = connection;
